d9228751dc449f5c1faf33a4b611bbbd6c3b3305
andy922200-m1 <andy922200@gmail.com>
2024-02-09 11:31:30 +0800

    chore: build settings

---
+++ src/main.ts
@@ -0,0 +1,65 @@
+import Accordion from './components/Accordion/Accordion'
+import Badge from './components/Badge/Badge'
+import Breadcrumbs from './components/Breadcrumbs/Breadcrumbs'
+import Button from './components/Button/Button'
+import Card from './components/Card/Card'
+import Carousel from './components/Carousel/Carousel'
+import Checkbox from './components/Checkbox/Checkbox'
+import Chip from './components/Chip/Chip'
+import Drawer from './components/Drawer/Drawer'
+import Dropdown from './components/Dropdown/Dropdown'
+import FormControl from './components/FormControl/FormControl'
+import {
+  InfiniteListenOnScroll,
+  InfiniteScrollObserver as InfiniteScroll,
+} from './components/InfiniteScroll/InfiniteScroll'
+import Modal from './components/Modal/Modal'
+import Pagination from './components/Pagination/Pagination'
+import ProgressBar from './components/ProgressBar/ProgressBar'
+import ProgressCircle from './components/ProgressCircle/ProgressCircle'
+import Radio from './components/Radio/Radio'
+import Rate from './components/Rate/Rate'
+import Select from './components/Select/Select'
+import Skeleton from './components/Skeleton/Skeleton'
+import Slider from './components/Slider/Slider'
+import Spin from './components/Spin/Spin'
+import Switch from './components/Switch/Switch'
+import Table from './components/Table/Table'
+import Tabs from './components/Tabs/Tabs'
+import TextField from './components/TextField/TextField'
+import Toast from './components/Toast/Toast'
+import Tooltip from './components/Tooltip/Tooltip'
+import Upload from './components/Upload/Upload'
+
+export {
+  Accordion,
+  Badge,
+  Breadcrumbs,
+  Button,
+  Card,
+  Carousel,
+  Checkbox,
+  Chip,
+  Drawer,
+  Dropdown,
+  FormControl,
+  InfiniteListenOnScroll,
+  InfiniteScroll,
+  Modal,
+  Pagination,
+  ProgressBar,
+  ProgressCircle,
+  Radio,
+  Rate,
+  Select,
+  Skeleton,
+  Slider,
+  Spin,
+  Switch,
+  Table,
+  Tabs,
+  TextField,
+  Toast,
+  Tooltip,
+  Upload,
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,35 @@
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ insertTypesEntry: true })],
   resolve: {
     alias: [{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }],
+  },
+  build: {
+    copyPublicDir: false,
+    lib: {
+      // this is the file that exports our components
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'main',
+      fileName: 'main',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
   },
 })
